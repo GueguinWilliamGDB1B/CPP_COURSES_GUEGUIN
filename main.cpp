c2eb9386dd4c62d16406c09092c114813b4bd419
GueguinWilliamGDB1B <74670003+GueguinWilliamGDB1B@users.noreply.github.com>
2021-03-10 16:55:13 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ using namespace std;
 
 int main(){
 
-    Courses Course1 = Courses("France");
+    Courses Course1 = Courses("France"){
+        score1 = 220;
+        score2 = 250;
+        score3 = 325;
+        scorej = 241;
+    };
     Courses Course2 = Courses("Australie");
     Courses Course3 = Courses("Egypte");
     Courses Course4 = Courses("Chine");
     Courses Course5 = Courses("Canada");
 
+    
 
 
 return 0;  
